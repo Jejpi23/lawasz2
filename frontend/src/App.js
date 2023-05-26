3b1a20c3b0237b44135881c479f96fd82c4efbc4
Jejpi23 <bogucki.szymon@wp.pl>
2023-05-26 14:49:43 +0200

    Nawigacja

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import Data from './Data';
+import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
+import Authors from './Authors';
+import Books from './Books';
+import Home from './Home';
 import './App.css';
 const App = () => {
   return (
-    <div>
-      <h1>My Library</h1>
-      <Data />
-    </div>
+    <Router>
+      <div className="container">
+        <nav className="navbar">
+          <Link to="/">Home</Link>
+          <Link to="/authors">Authors</Link>
+          <Link to="/books">Books</Link>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/authors" element={<Authors />} />
+          <Route path="/books" element={<Books />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
---
+++ frontend/src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="welcome">Welcome to the Library App!</h1>
+      <p>This is the home page of the library app.</p>
+    </div>
+  );
+};
+
+export default Home;
